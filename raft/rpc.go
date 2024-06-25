518ec4eb9d98706350eefaa662ccde1055237d61
Ryan <34078850+YourAverageMoron@users.noreply.github.com>
2024-06-25 07:49:14 +0100

    Add AppendEntriesRPC structs

--- raft/rpc.go
+++ raft/rpc.go
@@ -9,3 +9,21 @@ type RegisterPeerRPC struct {
 	AdvertisedAddr string
 	Network        string
 }
+
+type LogEntry struct {
+ //TODO: WHAT SHOULD BE STORED IN THIS?
+}
+
+type AppendEntriesRPCRequest struct {
+ Term int32
+ LeaderId net.Addr
+ PrevLogIndex int32
+ PrevLogTerm int32
+ Entries []LogEntry
+ LeaderCommit int32
+}
+
+type AppendEntriesRPCResponse struct {
+ Term int32
+ Success bool
+}
