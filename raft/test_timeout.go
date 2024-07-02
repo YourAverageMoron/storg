72cfe5b4e1d8592faed53e4846f7a90c381aab63
YourAverageMoron <ryannffc21@gmail.com>
2024-07-02 21:15:34 +0100

    created test file for timeout

---
+++ raft/test_timeout.go
@@ -0,0 +1,7 @@
+package raft
+
+import "testing"
+
+func TestTimeout(t *testing.T) {
+
+}
--- raft/timeout.go
+++ raft/timeout.go
@@ -1,61 +1,62 @@
 package raft
 
 import (
-    "fmt"
-    "math/rand/v2"
-    "time"
+	"fmt"
+	"math/rand/v2"
+	"time"
 )
 
 func ElectionTimeoutFunc() time.Duration {
-  ms := rand.IntN(150) + 150
-  return time.Duration(ms) * time.Millisecond
+	ms := rand.IntN(150) + 150
+	return time.Duration(ms) * time.Millisecond
 }
 
 type Timeout struct {
-  durationFunc func() time.Duration
-  resetch chan struct{}
-  timeoutch chan struct{}
-  quitch chan struct{}
+	durationFunc func() time.Duration
+	resetch      chan struct{}
+	timeoutch    chan struct{}
+	quitch       chan struct{}
 }
 
 func NewTimeout(durationFunc func() time.Duration) *Timeout {
-  resetch := make(chan struct{})
-  timeoutch := make(chan struct{})
-  quitch := make(chan struct{})
-  return &Timeout{
-    durationFunc,
-    resetch,
-    timeoutch,
-    quitch,
-  }
+	resetch := make(chan struct{})
+	timeoutch := make(chan struct{})
+	quitch := make(chan struct{})
+	return &Timeout{
+		durationFunc,
+		resetch,
+		timeoutch,
+		quitch,
+	}
 }
 
 func (t *Timeout) Start() {
-  go t.loop()
+	go t.loop()
 }
 
 func (t *Timeout) Consume() <-chan struct{} {
-  return t.timeoutch
+	return t.timeoutch
 }
 
 func (t *Timeout) Stop() {
-  t.quitch <- struct{}{}
+	t.quitch <- struct{}{}
 }
 
 func (t *Timeout) Reset() {
-  t.resetch <- struct{}{}
+	t.resetch <- struct{}{}
 }
 
 func (t *Timeout) loop() {
-  for {
-    select {
-    case <-t.quitch:
-      return
-    case <-t.resetch:
-        fmt.Println("received reset command, resetting timeout")
-    case <-time.After(t.durationFunc()):
-        fmt.Println("timeout exceeded, sending timeout message")
-        t.timeoutch <- struct{}{}
-    }
-  }
+	for {
+		select {
+		case <-t.quitch:
+			return
+		case <-t.resetch:
+			fmt.Println("received reset command, resetting timeout")
+		case <-time.After(t.durationFunc()):
+			fmt.Println("timeout exceeded, sending timeout message")
+			t.timeoutch <- struct{}{}
+		}
+	}
 }
+
